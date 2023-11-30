afa1a404d81aa1c3347a0c880e3eceedd0f159d6
kkamara <kamaracomputers@gmail.com>
2023-11-30 12:33:33 +0000

    Stop serving react app

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,3 @@ use App\Jobs\TestJob;
 |
 */
 
-Route::get('/job', function() {
-    Log::debug('here');
-    TestJob::dispatch()
-        ->onConnection('redis')
-        ->onQueue('stuff');
-    return response('Success');
-});
-
-Route::get('/email', function () {
-    Log::debug('test');
-    
-    $email = new TestMail();
-    Mail::to('recipient@localhost')->send($email);
-    return $email;
-});
-
-Route::view('/{path?}', 'layouts.app')->where('path', '.*');
