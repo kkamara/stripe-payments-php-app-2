24fffae7574cf5ebbcfb34d861df4ef26b71420e
kkamara <kamaracomputers@gmail.com>
2023-11-30 12:44:08 +0000

    Add Products database fields

--- database/migrations/2023_11_30_123858_create_products_table.php
+++ database/migrations/2023_11_30_123858_create_products_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string("name", 255);
+            $table->decimal("price", 6, 2);
+            $table->string("image", 1024);
             $table->timestamps();
         });
     }
